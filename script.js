775e3b5f5b5a14040a0c0dab4cff72e00b7748c8
Ivona Zelenska <ney5744@gmail.com>
2024-11-25 13:58:30 +0000

    task 2

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ for (let i = 1; i <= 100; i++) {
     }
 }
 
+// завдання 2
+
+//if
+
+function printEvenNumbers(value) {
+    if (typeof value !== 'number' || isNaN(value)) {
+        console.log('Таке чуство шо Бог десь наказує нас за шось');
+        return;
+    }
+
+    for (let i = 2; i < value; i += 2) {
+        console.log(i);
+    }
+}
+
+//while
+
+function printEvenNumbersWhile(value) {
+    if (typeof value !== 'number' || isNaN(value)) {
+        console.log('Таке чуство шо Бог десь наказує нас за шось');
+        return;
+    }
+
+    let i = 2;
+    while (i < value) {
+        console.log(i);
+        i += 2;
+    }
+}
